305eb6a4d99336c7ad78945484046b8fbc261087
Hankscr459 <defcw58@gmail.com>
2022-02-27 18:11:34 +0800

    Creating the Connection string

--- API/Enities/AppUser.cs
+++ API/Enities/AppUser.cs
@@ -4,11 +4,5 @@ namespace API.Enities
     {
         public int Id { get; set; }
         public string? UserName { get; set; }
-        private int myVar;
-        public int MyProperty 
-        { 
-            get { return myVar; }
-            set { myVar = value; }
-        }
     }
 }
