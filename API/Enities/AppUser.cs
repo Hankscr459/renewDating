e189c48f9fc965e30234d74b660d930118d9d65e
Hankscr459 <defcw58@gmail.com>
2022-02-27 15:21:51 +0800

    Adding a DbContext class

--- API/Enities/AppUser.cs
+++ API/Enities/AppUser.cs
@@ -3,7 +3,7 @@ namespace API.Enities
     public class AppUser
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string? UserName { get; set; }
         private int myVar;
         public int MyProperty 
         { 
