09b9b13fbedb0272835ceb03fec2f9f5705acf52
Hankscr459 <defcw58@gmail.com>
2022-02-27 11:01:37 +0800

    Creating our first Entity

---
+++ API/Enities/AppUser.cs
@@ -0,0 +1,14 @@
+namespace API.Enities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        private int myVar;
+        public int MyProperty 
+        { 
+            get { return myVar; }
+            set { myVar = value; }
+        }
+    }
+}
