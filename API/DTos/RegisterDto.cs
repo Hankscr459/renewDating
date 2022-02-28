45d38b83c25092d72a70872c5736394e71c5fc8a
Hankscr459 <defcw58@gmail.com>
2022-02-28 15:05:47 +0800

    Adding validation

--- API/DTos/RegisterDto.cs
+++ API/DTos/RegisterDto.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTos
 {
     public class RegisterDto
     {
+        [Required(ErrorMessage="使用者名稱欄未必填")]
         public string Username { get; set; }
+        [Required(ErrorMessage="密碼欄未必填")]
         public string Password { get; set; }
     }
 }
