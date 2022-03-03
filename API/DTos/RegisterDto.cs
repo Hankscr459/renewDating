3d64de339913440a4fd3312f0eaf79338f6a78af
Hankscr459 <defcw58@gmail.com>
2022-03-03 22:59:34 +0800

    Creating an error controller for testing errors

--- API/DTos/RegisterDto.cs
+++ API/DTos/RegisterDto.cs
@@ -7,6 +7,7 @@ namespace API.DTOs
         [Required(ErrorMessage="使用者名稱欄未必填")]
         public string Username { get; set; }
         [Required(ErrorMessage="密碼欄未必填")]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
