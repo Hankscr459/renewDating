aac73db4638b5994e0e46a5345426730e87b4cb2
hank <akysn564@gmail.com>
2022-03-18 13:14:55 +0800

    Adding a presence tracker

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -7,13 +7,24 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
-        public override async Task OnConnectedAsync ()
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
         {
-            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+            _tracker = tracker;
         }
 
-        public override async Task OnDisconnectedAsync (Exception exception)
+        public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+            await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.All.SendAsync("GetOnlineUsers", currentUsers);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
             await base.OnDisconnectedAsync(exception);
         }
